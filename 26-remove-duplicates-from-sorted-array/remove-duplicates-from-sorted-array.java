6b13a77a51d824689520d5ce87bae58f518c087d
Adilah Auni <dylaoni33@gmail.com>
2026-02-06 00:14:50 +0800

    Time: 1 ms (77.57%) | Memory: 46.6 MB (78.00%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int j=0;
-        for(int i = 1; i<nums.length;i++){
-            if(nums[j] != nums[i]){
-                j++;
-                nums [j] = nums [i];
+        int j=0; //keeps track of the last unique number’s position aka index. "unique marker"
+        for(int i = 1; i<nums.length;i++){ //i start scan from 2nd element bcs it needs to compare the num to previous element whhich is the 1st element
+            if(nums[j] != nums[i]){ //if current nums is diff than previous nums, then new unique nums is found
+                j++; //move unique marker forward
+                nums [j] = nums [i]; //put new unique nums there
             }
         }
-        return j+1;
+        return j+1; //return the number of unique elements. E.g: last unique index is 2 (0,1,2), the count is 3
     }
 }
